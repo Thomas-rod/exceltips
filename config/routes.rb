1ec5c9cb3cd689b1c3fff1cb867edc71a8296638
thomas <rodiert17@gmail.com>
2020-03-25 19:41:43 +0100

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:show, :index, :create, :update]
-  resources :courses, only: [:index, :show] do
-    resources :comments, only: [:index, :create, :update, :destroy]
-    resources :answers, only: [:index, :create, :update, :destroy]
+  resources :users, only: [:show] do
+    resources :ratings, only: [:show]
   end
-  resources :rating, only: [:index, :show, :create, :update]
-  resources :courses, only: [:update, :create]
+  resources :courses, only: [:index]
+  resources :payments, only: [:create, :show]
 end
