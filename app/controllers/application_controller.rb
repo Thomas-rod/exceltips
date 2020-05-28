c59d1656ec88e6a0605ba1003cc1b0b72553e7cf
thomas rodier <rodiert17@gmail.com>
2020-05-28 15:26:56 +0200

    now can share on facebook with meta_tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,11 @@ class ApplicationController < ActionController::Base
   # Uncomment when you *really understand* Pundit!
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
+
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
   protected
 
   def after_sign_in_path_for(resource)
